757b288ce01ba40191e667aa3a1581e466e63991
Pradeep-kefcom <49935026+Pradeep-kefcom@users.noreply.github.com>
2019-09-13 13:56:02 +0530

    Intial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dummy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EF")]
+[assembly: AssemblyProduct("Dummy")]
+[assembly: AssemblyCopyright("Copyright © EF 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5649e3b0-a227-40a8-ab31-0cf97ec34225")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dummy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Test 1 Method");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("Test 2 Method");
+        }
+
+    }
+}
